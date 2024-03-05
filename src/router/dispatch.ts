aef5fe44ced796b9e3e930f131fab11c957ae1e1
LanceGin <gin.lance.inside@hotmail.com>
2024-03-06 04:50:01 +0800

    test: 派车单新逻辑

--- src/router/dispatch.ts
+++ src/router/dispatch.ts
@@ -134,23 +134,23 @@ const importDispatchList = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(401).end();
   }
-  let sql: string = "select * from container where container_status in ('运输中','已完成') and order_type = '进口' and temp_status = '未暂落' ";
-  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and DATE_FORMAT(make_time,'%Y%m%d') between " + "DATE_FORMAT('" + form.make_time_range[0] + "','%Y%m%d') and DATE_FORMAT('" + form.make_time_range[1] + "','%Y%m%d')" }
-  if (form.track_no != "") { sql += " and track_no like " + "'%" + form.track_no + "%'" }
-  if (form.door != "") { sql += " and door like " + "'%" + form.door + "%'" }
-  if (form.container_type != "") { sql += " and container_type = " + "'" + form.container_type + "'" }
-  if (form.containner_no != "") { sql += " and containner_no like " + "'%" + form.containner_no + "%'" }
-  if (form.car_no != "") { sql += " and car_no like " + "'%" + form.car_no + "%'" }
-  if (form.container_status != "") { sql += " and container_status like " + "'%" + form.container_status + "%'" }
+  let sql: string = "select a.id as dispatch_id, a.type, a.status, a.car_no as dispatch_car_no, a.trans_status, b.* from dispatch as a left join container as b on b.id = a.container_id where a.type = '拆箱' and a.status = '已派车' ";
+  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and DATE_FORMAT(b.make_time,'%Y%m%d') between " + "DATE_FORMAT('" + form.make_time_range[0] + "','%Y%m%d') and DATE_FORMAT('" + form.make_time_range[1] + "','%Y%m%d')" }
+  if (form.track_no != "") { sql += " and b.track_no like " + "'%" + form.track_no + "%'" }
+  if (form.door != "") { sql += " and b.door like " + "'%" + form.door + "%'" }
+  if (form.container_type != "") { sql += " and b.container_type = " + "'" + form.container_type + "'" }
+  if (form.containner_no != "") { sql += " and b.containner_no like " + "'%" + form.containner_no + "%'" }
+  if (form.car_no != "") { sql += " and a.car_no like " + "'%" + form.car_no + "%'" }
+  if (form.container_status != "") { sql += " and b.container_status like " + "'%" + form.container_status + "%'" }
   sql +=" order by id desc limit " + size + " offset " + size * (page - 1);
-  sql +=";select COUNT(*) from container where container_status in ('运输中','已完成') and order_type = '进口' and temp_status = '未暂落' ";
-  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and DATE_FORMAT(make_time,'%Y%m%d') between " + "DATE_FORMAT('" + form.make_time_range[0] + "','%Y%m%d') and DATE_FORMAT('" + form.make_time_range[1] + "','%Y%m%d')" }
-  if (form.track_no != "") { sql += " and track_no like " + "'%" + form.track_no + "%'" }
-  if (form.door != "") { sql += " and door like " + "'%" + form.door + "%'" }
-  if (form.container_type != "") { sql += " and container_type = " + "'" + form.container_type + "'" }
-  if (form.containner_no != "") { sql += " and containner_no like " + "'%" + form.containner_no + "%'" }
-  if (form.car_no != "") { sql += " and car_no like " + "'%" + form.car_no + "%'" }
-  if (form.container_status != "") { sql += " and container_status like " + "'%" + form.container_status + "%'" }
+  sql +=";select COUNT(*) from dispatch as a left join container as b on b.id = a.container_id where a.type = '拆箱' and a.status = '已派车' ";
+  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and DATE_FORMAT(b.make_time,'%Y%m%d') between " + "DATE_FORMAT('" + form.make_time_range[0] + "','%Y%m%d') and DATE_FORMAT('" + form.make_time_range[1] + "','%Y%m%d')" }
+  if (form.track_no != "") { sql += " and b.track_no like " + "'%" + form.track_no + "%'" }
+  if (form.door != "") { sql += " and b.door like " + "'%" + form.door + "%'" }
+  if (form.container_type != "") { sql += " and b.container_type = " + "'" + form.container_type + "'" }
+  if (form.containner_no != "") { sql += " and b.containner_no like " + "'%" + form.containner_no + "%'" }
+  if (form.car_no != "") { sql += " and a.car_no like " + "'%" + form.car_no + "%'" }
+  if (form.container_status != "") { sql += " and b.container_status like " + "'%" + form.container_status + "%'" }
   connection.query(sql, async function (err, data) {
     if (err) {
       Logger.error(err);
@@ -337,23 +337,23 @@ const tempDropDispatchList = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(401).end();
   }
-  let sql: string = "select * from container where container_status in ('运输中','已完成') and temp_status = '已暂落' ";
-  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and make_time between " + "'" + form.make_time_range[0] + "' and '" + form.make_time_range[1] + "'" }
-  if (form.track_no != "") { sql += " and track_no like " + "'%" + form.track_no + "%'" }
-  if (form.door != "") { sql += " and door like " + "'%" + form.door + "%'" }
-  if (form.container_type != "") { sql += " and container_type = " + "'" + form.container_type + "'" }
-  if (form.containner_no != "") { sql += " and containner_no like " + "'%" + form.containner_no + "%'" }
-  if (form.car_no != "") { sql += " and car_no like " + "'%" + form.car_no + "%'" }
-  if (form.container_status != "") { sql += " and container_status like " + "'%" + form.container_status + "%'" }
+  let sql: string = "select a.id as dispatch_id, a.type, a.status, a.car_no as dispatch_car_no, a.trans_status, b.* from dispatch as a left join container as b on b.id = a.container_id where a.type = '暂落' and a.status = '已派车' ";
+  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and DATE_FORMAT(b.make_time,'%Y%m%d') between " + "DATE_FORMAT('" + form.make_time_range[0] + "','%Y%m%d') and DATE_FORMAT('" + form.make_time_range[1] + "','%Y%m%d')" }
+  if (form.track_no != "") { sql += " and b.track_no like " + "'%" + form.track_no + "%'" }
+  if (form.door != "") { sql += " and b.door like " + "'%" + form.door + "%'" }
+  if (form.container_type != "") { sql += " and b.container_type = " + "'" + form.container_type + "'" }
+  if (form.containner_no != "") { sql += " and b.containner_no like " + "'%" + form.containner_no + "%'" }
+  if (form.car_no != "") { sql += " and a.car_no like " + "'%" + form.car_no + "%'" }
+  if (form.container_status != "") { sql += " and b.container_status like " + "'%" + form.container_status + "%'" }
   sql +=" order by id desc limit " + size + " offset " + size * (page - 1);
-  sql +=";select COUNT(*) from container where container_status in ('运输中','已完成') and temp_status = '已暂落' ";
-  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and make_time between " + "'" + form.make_time_range[0] + "' and '" + form.make_time_range[1] + "'" }
-  if (form.track_no != "") { sql += " and track_no like " + "'%" + form.track_no + "%'" }
-  if (form.door != "") { sql += " and door like " + "'%" + form.door + "%'" }
-  if (form.container_type != "") { sql += " and container_type = " + "'" + form.container_type + "'" }
-  if (form.containner_no != "") { sql += " and containner_no like " + "'%" + form.containner_no + "%'" }
-  if (form.car_no != "") { sql += " and car_no like " + "'%" + form.car_no + "%'" }
-  if (form.container_status != "") { sql += " and container_status like " + "'%" + form.container_status + "%'" }
+  sql +=";select COUNT(*) from dispatch as a left join container as b on b.id = a.container_id where a.type = '暂落' and a.status = '已派车' ";
+  if (form.make_time_range && form.make_time_range.length > 0) { sql += " and DATE_FORMAT(b.make_time,'%Y%m%d') between " + "DATE_FORMAT('" + form.make_time_range[0] + "','%Y%m%d') and DATE_FORMAT('" + form.make_time_range[1] + "','%Y%m%d')" }
+  if (form.track_no != "") { sql += " and b.track_no like " + "'%" + form.track_no + "%'" }
+  if (form.door != "") { sql += " and b.door like " + "'%" + form.door + "%'" }
+  if (form.container_type != "") { sql += " and b.container_type = " + "'" + form.container_type + "'" }
+  if (form.containner_no != "") { sql += " and b.containner_no like " + "'%" + form.containner_no + "%'" }
+  if (form.car_no != "") { sql += " and a.car_no like " + "'%" + form.car_no + "%'" }
+  if (form.container_status != "") { sql += " and b.container_status like " + "'%" + form.container_status + "%'" }
   connection.query(sql, async function (err, data) {
     if (err) {
       Logger.error(err);
