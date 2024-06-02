e4f4f9b672ae523663c777f9254feec28f2a0522
LanceGin <gin.lance.inside@hotmail.com>
2024-06-02 17:29:19 +0800

    test: 费用申请人为提交人

--- src/router/finance.ts
+++ src/router/finance.ts
@@ -513,7 +513,7 @@ const financeCheckList = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(401).end();
   }
-  let sql: string = `SELECT a.id,a.type,a.status,a.fee_name,a.account_period,a.custom_name,a.project_name,a.flow_direction,a.acc_company,a.content,a.is_invoice,a.remark,c.company_name,c.bank,c.account_no,b.container_type,b.add_by,FORMAT(sum(a.amount),2) as amount,FORMAT(sum(a.less_amount),2) as less_amount,FORMAT(sum(a.more_amount),2) as more_amount,FORMAT(sum(a.amount-a.less_amount+a.more_amount),2) as actual_amount,count(b.id) as total, COUNT(IF(left(b.container_type, 2) = '40',true,null)) as f, COUNT(IF(left(b.container_type, 2) = '20',true,null)) as t FROM container_fee as a left join container as b on a.container_id = b.id left join acc_company as c on c.id = a.acc_company where a.id is not null`;
+  let sql: string = `SELECT a.id,a.type,a.status,a.fee_name,a.account_period,a.custom_name,a.project_name,a.flow_direction,a.acc_company,a.content,a.is_invoice,a.remark,a.submit_by,c.company_name,c.bank,c.account_no,b.container_type,b.add_by,FORMAT(sum(a.amount),2) as amount,FORMAT(sum(a.less_amount),2) as less_amount,FORMAT(sum(a.more_amount),2) as more_amount,FORMAT(sum(a.amount-a.less_amount+a.more_amount),2) as actual_amount,count(b.id) as total, COUNT(IF(left(b.container_type, 2) = '40',true,null)) as f, COUNT(IF(left(b.container_type, 2) = '20',true,null)) as t FROM container_fee as a left join container as b on a.container_id = b.id left join acc_company as c on c.id = a.acc_company where a.id is not null`;
   if (form.type != "") { sql += " and a.type = " + "'" + form.type + "'" }
   if (form.status != "") { sql += " and a.status = " + "'" + form.status + "'" }
   if (form.custom_name != "") { sql += " and a.custom_name = " + "'" + form.custom_name + "'" }
@@ -1277,7 +1277,7 @@ const approvePay = async (req: Request, res: Response) => {
     content,
     actual_amount,
     remark,
-    add_by
+    submit_by
   }  = req.body;
   let payload = null;
   const status = '已审核';
@@ -1302,7 +1302,7 @@ const approvePay = async (req: Request, res: Response) => {
       const is_pay = "付";
       const add_time = dayjs(new Date()).format("YYYY-MM-DD");
       const fee_no = "FAO" + dayjs(new Date()).format("YYYYMMDD") + Math.floor(Math.random()*10000);
-      let apply_fee_sql: string = `insert into applied_fee (is_admin,fee_name,is_pay,apply_amount,acc_company_id,apply_by,create_time,fee_no,remark) values ('${is_admin}','${content}','${is_pay}','${actual_amount}','${acc_company}','${add_by}','${add_time}','${fee_no}','${remark}');`;
+      let apply_fee_sql: string = `insert into applied_fee (is_admin,fee_name,is_pay,apply_amount,acc_company_id,apply_by,create_time,fee_no,remark) values ('${is_admin}','${content}','${is_pay}','${actual_amount}','${acc_company}','${submit_by}','${add_time}','${fee_no}','${remark}');`;
       connection.query(apply_fee_sql, async function (err, data) {
         if (err) {
           console.log(err);
