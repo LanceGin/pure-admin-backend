dc0aa976c22a4739e3b1c292c50f56da010c9dbc
LanceGin <gin.lance.inside@hotmail.com>
2024-06-20 11:29:32 +0800

    test: 异常费用备注同步到费用

--- src/router/operation.ts
+++ src/router/operation.ts
@@ -657,8 +657,8 @@ const addContainerFee = async (req: Request, res: Response) => {
     return res.status(401).end();
   }
   let sql: string = `insert into dispatch_fee (dispatch_id, fee_name, fee, remark) values ('${dispatch_id}', '${fee_name}', '${fee}', '${remark}');`;
-  sql += `insert into container_fee (container_id, type, fee_name, amount) values ('${id}', '应付', '${fee_name}', '${fee}');`;
-  sql += `insert into container_fee (container_id, type, fee_name, amount) values ('${id}', '应收', '${fee_name}', '${fee}');`;
+  sql += `insert into container_fee (container_id, type, fee_name, amount, remark) values ('${id}', '应付', '${fee_name}', '${fee}'), '${remark}');`;
+  sql += `insert into container_fee (container_id, type, fee_name, amount, remark) values ('${id}', '应收', '${fee_name}', '${fee}'), '${remark}');`;
   connection.query(sql, async function (err, data) {
     if (err) {
       console.log(err);
